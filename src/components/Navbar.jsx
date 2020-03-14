695f8401ce3a334baa82f2d59755bc5ed9872880
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-03-14 16:07:38 +0100

    Add new logo, card deck design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style/Navbar.css';
-import PokeBallImage from '../resources/pokeball.png';
+import Logo from '../resources/logo.png';
 import { Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = props => {
   return (
     <nav>
-      <img src={PokeBallImage} alt="icon" />
+      <img className="logo" src={Logo} alt="icon" />
       <h1>Reactomon</h1>
       <Nav className="justify-content-center">
         <Nav.Item>
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,34 +1,59 @@
 import React, { Component } from 'react';
 import { Card, Button, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './style/PokemonCard.css';
-import CardBack from '../resources/cardback.png';
+import OpenPokeBall from '../resources/openpokeball.png';
 import Gotta from '../resources/gotta.png';
+import ClosedPokeBall from '../resources/closedpokeball.png';
+import './style/PokemonCard.css';
 
 class PokemonCard extends Component {
+  state = {
+    backimg: ClosedPokeBall
+  };
+
+  handleMouseOver(view) {
+    if (view === 'front') {
+      this.setState({
+        backimg: OpenPokeBall
+      });
+    } else {
+      this.setState({
+        backimg: ClosedPokeBall
+      });
+    }
+  }
+
   render() {
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={CardBack} />
-        <Card.Body>
-          <Card.Title>
-            {this.props.pokemon.name.charAt(0).toUpperCase() +
-              this.props.pokemon.name.slice(1)}
-          </Card.Title>
-          <Card.Text>
-            <Button>
-              <Image src={Gotta} fluid />
-            </Button>
-          </Card.Text>
+      <Card className="cardback">
+        <div
+          onMouseOver={() => this.handleMouseOver('front')}
+          onMouseOut={() => this.handleMouseOver('back')}
+        >
           <Link
             to={{
               pathname: `/pokemon/${this.props.pokemon.name}`,
               state: { url: this.props.pokemon.url }
             }}
           >
-            Detailed view
+            <Card.Img
+              className="backcard-img"
+              variant="top"
+              src={this.state.backimg}
+            />
           </Link>
+        </div>
+        <Card.Body>
+          <Card.Title className="text-center">
+            {this.props.pokemon.name.charAt(0).toUpperCase() +
+              this.props.pokemon.name.slice(1)}
+          </Card.Title>
         </Card.Body>
+        <Card.Footer className="float-right">
+          <Button>
+            <Image className="catchimg" src={Gotta} fluid />
+          </Button>
+        </Card.Footer>
       </Card>
     );
   }
