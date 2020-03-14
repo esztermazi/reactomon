35acd56c7102f921edcf41382fa8fd417a5f7d6c
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-03-14 18:26:50 +0100

    Implement hooked PokemonList.jsx

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PokemonCard from './PokemonCard';
 
-class PokemonList extends Component {
-  render() {
-    return this.props.pokemons.map(pokemon => (
-      <PokemonCard pokemon={pokemon} key={pokemon.name} />
-    ));
-  }
-}
+const PokemonList = props => {
+  const content = props.pokemons.map(pokemon => (
+    <PokemonCard pokemon={pokemon} key={pokemon.name} />
+  ));
+  return content;
+};
 
 export default PokemonList;
